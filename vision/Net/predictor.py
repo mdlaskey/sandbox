3d1423b1b717bab6ec0d12cbf2eb9f1db2704ad9
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-12-06 17:34:59 -0800

    continued testing net architectures

--- vision/Net/predictor.py
+++ vision/Net/predictor.py
@@ -3,21 +3,25 @@ import hdf
 import numpy as np
 import os
 
-MODEL = './model.prototxt'
-WEIGHTS = './weights_iter_100.caffemodel'
+MODEL = './nets/net3/model3.prototxt'
+WEIGHTS = './weights_iter_110.caffemodel'
 
 net = caffe.Net(MODEL, WEIGHTS, caffe.TEST)
 
-for filename in os.listdir('./images_test'):
-    if filename.endswith('.jpg'):
-        image = caffe.io.load_image('./images_test/' + filename)
-        #image = hdf.reshape(image)
+f = open('./hdf/test.txt', 'r')
 
-        data4D = np.zeros([1,3,250,250])
-        data4D[0,0,:,:] = image[:,:,0]
-	data4D[0,1,:,:] = image[:,:,1]
-	data4D[0,2,:,:] = image[:,:,2]
+for line in f:
+    path = line.split(' ')[0]
+    image = caffe.io.load_image(path)
 
-        net.forward_all(data=data4D)
-        data = net.blobs['fc2'].data.copy()
-        print filename + str([ 150.0 * x for x in data[0] ])
+    data4D = np.zeros([1,3,125,125])
+    data4D[0,0,:,:] = image[:,:,0]
+    data4D[0,1,:,:] = image[:,:,1]
+    data4D[0,2,:,:] = image[:,:,2]
+
+    net.forward_all(data=data4D)
+    data = net.blobs['fc2'].data.copy()
+    s = ""
+    for x in data[0]:
+        s += " " + str(x)
+    print path + s
