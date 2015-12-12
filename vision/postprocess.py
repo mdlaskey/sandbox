e31a3c2c2efdc6ef6b2e4cf614e6e7fe0db8a99a
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-12-11 16:12:35 -0800

    added ability to aggregate datasets to postprocess.py

--- vision/postprocess.py
+++ vision/postprocess.py
@@ -23,23 +23,31 @@ reader = open(ds_path + "controls.txt", 'r')
 open("Net/hdf/train.txt", 'w').close()
 open("Net/hdf/test.txt", 'w').close()
 
-for line in reader:
-    split = line.split(' ')
-    filename = split[0]
-    controls = [ float(x)/float(s) for x, s in zip(split[1:], gopt.GripperOptions.scales) ]
+train_writer = open("Net/hdf/train.txt", "w+")
+test_writer = opne("Net/hdf/test.txt", "w+")
 
-    if args['segment']:
-        im = cv2.imread(ds_path + filename)
-        im = bc.pipe(im)
-        cv2.imwrite("./net/images/" + filename, im)
-    
-    controls_string = ""
-    for c in controls: controls_string += " " + str(c)
-    line = constants.ROOT + 'images/' + filename + controls_string + "\n"
-    
-    if random.random() > .2:
-        train_writer.write(line)
-    else:
-        test_writer.write(line)
+readers = [ open(ds_path + "controls.txt") for ds_path in ds_paths ]
+
+for reader, ds_path in zip(readers, ds_paths):
+    bc = bincam.BinaryCamera(ds_path + "meta.txt")
+    for line in reader:
+        split = line.split(' ')
+        filename = split[0]
+        new_filename = ds_path + "_" + filename
+        controls = [ float(x)/float(s) for x, s in zip(split[1:], gopt.GripperOptions.scales) ]
+
+        if args['segment']:
+            im = cv2.imread(ds_path + filename)
+            im = bc.pipe(im)
+            cv2.imwrite("./net/images/" + new_filename, im)
+        
+        controls_string = ""
+        for c in controls: controls_string += " " + str(c)
+        line = constants.ROOT + 'images/' + new_filename + controls_string + "\n"
+        
+        if random.random() > .2:
+            train_writer.write(line)
+        else:
+            test_writer.write(line)
 
 
